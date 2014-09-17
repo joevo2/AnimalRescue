184d310e0c643b55f011f5737cab1507bc19bfa0
Lee Hanxue <hanxue.developer@gmail.com>
2014-09-17 21:52:56 +0800

    Add Auth0 authentication
    Please refer to http://www.auth0.com
    We are using the domain http://animalrescue.auth0.com
    Integration with: Facebook, Google+, Twitter
    Auth0 will handle all backend authentication

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'auth0'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,7 +21,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $authProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -79,6 +79,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
+
+  authProvider.init({
+    domain:       'animalrescue.auth0.com',
+    clientID:     'pIuGHl4YRA0Xef5UIIVGRgtfJ5xNtPIO',
+    callbackURL:  location.href,
+    loginState:   'login'
+  })
+  .run(function(auth) {
+    // Hook auth0-angular to all the events it needs to listen to
+    auth.hookEvents();
+  });
 
 });
 
